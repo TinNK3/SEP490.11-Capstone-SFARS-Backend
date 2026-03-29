4e249c1ba435e1dab3b5d4a87b3791ad5f057ef6
Nguyen Khanh Tin <khanhtin.work@gmail.com>
2026-03-29 10:04:19 +0700

    feat: Add descriptions to ChatSenderType, OtpType, and SosSpamStatus enums

--- SFARS.Domain/Common/Enum/SosSpamStatus.cs
+++ SFARS.Domain/Common/Enum/SosSpamStatus.cs
@@ -1,9 +1,14 @@
-﻿namespace SFARS.Domain.Common.Enum
+﻿using System.ComponentModel;
+
+namespace SFARS.Domain.Common.Enum
 {
     public enum SosSpamStatus
     {
+        [Description("Cho phép")]
         Allowed,
+        [Description("Cảnh báo nhẹ")]
         SoftWarning,
+        [Description("Chặn cứng")]
         HardBlocked
     }
 }
